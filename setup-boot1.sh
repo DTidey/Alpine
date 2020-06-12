72ce05413a41678e2c568100a1ce7368605ec622
DTidey <65171633+DTidey@users.noreply.github.com>
2020-06-12 16:04:42 +0100

    Update setup-boot1.sh

--- setup-boot1.sh
+++ setup-boot1.sh
@@ -1,1 +1,18 @@
-
+echo "welcome to the install script"
+#
+echo "Change root password"
+passwd
+#
+echo "Update package list"
+#
+apk update
+#
+echo "Add new user"
+#
+adduser david
+for u in $(ls /home); do for g in wheel disk lp floppy audio cdrom dialout video netdev games users; do addgroup $u $g; done;done
+#
+echo "Install Sudo"
+#
+apk add sudo
+#
