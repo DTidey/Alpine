8e392170397c6c4362e79cb0f8f7f51ed2c66e16
DTidey <65171633+DTidey@users.noreply.github.com>
2020-06-12 18:51:03 +0100

    Update setup-boot.sh

--- setup-boot.sh
+++ setup-boot.sh
@@ -1,3 +1,31 @@
+
+echo "welcome to the install script"
+#
+echo "Change root password"
+passwd
+#
+echo "Update package list"
+#
+apk update
+#
+echo "Install bash"
+apk add bash
+#
+echo "Add new user"
+#
+adduser --shell /bin/bash david
+for u in $(ls /home); do for g in wheel disk lp floppy audio cdrom dialout video netdev games users; do addgroup $u $g; done;done
+#
+echo "Install Sudo"
+apk add sudo
+#
+echo '%wheel ALL=(ALL) NOPASSWD: ALL' > /etc/sudoers.d/wheel
+#
+echo "Change MOTD"
+echo "Welecome to Alpine Linux" > /etc/motd
+#
+#
+#
 echo "Add standard packages"
 #
 echo "Install Hyper-V extensions"
@@ -16,6 +44,5 @@ rc-update add hv_fcopy_daemon
 rc-update add hv_kvp_daemon
 rc-update add hv_vss_daemon
 #
-
 
 
