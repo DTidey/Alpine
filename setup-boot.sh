7baa824351f158a243eb5db24630136358b45760
DTidey <65171633+DTidey@users.noreply.github.com>
2020-06-12 16:07:50 +0100

    Update setup-boot.sh

--- setup-boot.sh
+++ setup-boot.sh
@@ -1,19 +1,2 @@
-echo "welcome to the install script"
-#
-echo "Change root password"
-passwd
-#
-echo "Update package list"
-#
-apk update
-#
-echo "Add new user"
-#
-adduser david
-for u in $(ls /home); do for g in wheel disk lp floppy audio cdrom dialout video netdev games users; do addgroup $u $g; done;done
-#
-echo "Install Sudo"
-#
-apk add sudo
-#
+echo "david ALL=(ALL) NOPASSWD ALL" >> /etc/sudoers
 
