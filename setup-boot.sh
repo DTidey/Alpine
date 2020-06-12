a2f347affa9445f54ad926dddc031f18ff2e77e7
DTidey <65171633+DTidey@users.noreply.github.com>
2020-06-12 16:19:16 +0100

    Update setup-boot.sh

--- setup-boot.sh
+++ setup-boot.sh
@@ -1,2 +1,21 @@
-echo "david ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+echo "Add standard packages"
+#
+echo "Install Hyper-V extensions"
+#
+apk add hvtools
+#
+# Then enable the services
+#
+rc-service hv_fcopy_daemon start
+rc-service hv_kvp_daemon start
+rc-service hv_vss_daemon start
+#
+# Then to ensure these services start on boot
+#
+rc-update add hv_fcopy_daemon
+rc-update add hv_kvp_daemon
+rc-update add hv_vss_daemon
+#
+
+
 
